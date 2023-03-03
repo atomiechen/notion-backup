310092d92f7c546640f83c1e8de2235e82cd729a
Atomie CHEN <atomic_cwh@163.com>
2023-03-03 20:17:32 +0800

    Make 'timeout' and 'waitcount' keyword arguments

--- notion-backup.js
+++ notion-backup.js
@@ -26,15 +26,21 @@ See https://medium.com/@arturburtsev/automated-notion-backups-f6af4edc298d for
 notes on how to get that information.`);
 }
 
-let timeout = -1;  // default: no timeout
-if (process.argv.length >= 3) {  // read timeout (in seconds) from command-line argument
-  word = process.argv[2]
-  if (!isNaN(word)) {  // exclude cases like 3abcd
-    let n = parseInt(word)
-    if (!isNaN(n)) {  // exclude NaN
-      timeout = n * 1000
+let timeout = -1;  // in seconds, default: no timeout
+let waitcount = 5;  // default: 5 times
+const argsObj = {}
+process.argv.slice(2).forEach(arg => {
+    const [key, value] = arg.split('=')
+    // check if not empty strings, and if value is a number
+    if (key !== '' && value !== '' && !isNaN(value)) {
+        argsObj[key] = Number(value)
     }
-  }
+})
+if ('--timeout' in argsObj) {
+  timeout = argsObj['--timeout'] * 1000
+}
+if ('--waitcount' in argsObj) {
+  waitcount = argsObj['--waitcount']
 }
 
 async function post (endpoint, data) {
@@ -48,7 +54,7 @@ async function sleep (seconds) {
 }
 
 // formats: markdown, html
-async function exportFromNotion (format, timeout) {
+async function exportFromNotion (format, timeout, waitcount) {
   // try {
     let startTime = Date.now();
     console.log("Start exporting as " + format)
@@ -78,8 +84,8 @@ async function exportFromNotion (format, timeout) {
       if (timeout > 0 && Date.now() - startTime > timeout) {
         throw new Error("timeout reached: " + timeout/1000 + "s");
       }
-      if (failCount >= 5) {
-        throw new Error("fail count >= 5");
+      if (failCount >= waitcount) {
+        throw new Error("fail count >= " + waitcount);
         // break;
       }
       await sleep(10);
@@ -141,12 +147,12 @@ async function exportFromNotion (format, timeout) {
   // }
 }
 
-async function backup(format, timeout) {
+async function backup(format, timeout, waitcount) {
   let cwd = process.cwd()
     , pathDir = join(cwd, format)
     , pathFile = join(cwd, `${format}.zip`)
   ;
-  return exportFromNotion(format, timeout).then(() => {
+  return exportFromNotion(format, timeout, waitcount).then(() => {
     // rmdirSync(pathDir, { recursive: true });
     rmSync(pathDir, { recursive: true, force: true });
     mkdirSync(pathDir, { recursive: true });
@@ -157,11 +163,11 @@ async function backup(format, timeout) {
 
 async function run () {
   let errorCount = 0;
-  await backup('markdown', timeout).catch(err => {
+  await backup('markdown', timeout, waitcount).catch(err => {
     console.log(err);
     errorCount++;
   });
-  await backup('html', timeout).catch(err => {
+  await backup('html', timeout, waitcount).catch(err => {
     console.log(err);
     errorCount++;
   });
